d29c86a81dab1dfe49d83a3bae25df6b59f85736
Hyrremm <abyssinianconq@gmail.com>
2024-03-04 21:29:29 +0300

    c

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,30 +1,83 @@
 ﻿using Lab3;
+using Queue = Lab3.Queue<int>;
 
 class Program
 {
     public static void Main()
     {
-        SinglyLinkedList<int> list = new();
-        list.InsertAfter(null, 40);
-        list.InsertAfter(list.Last, 43);
-        list.InsertAfter(list.Last, 42433);
-        list.InsertAfter(list.Last, 431253);
-        list.InsertAfter(list.Last, 4343);
-        list.InsertAfter(list.Last, 43123);
-        Console.WriteLine("Printing all elements");
-        list.PrintAll();
-        (SinglyLinkedList<int>.Node foundPrev,SinglyLinkedList<int>.Node foundCur) = list.Find(43123);
-        Console.WriteLine("found prev - "+(foundPrev?.Data.ToString() ?? "none") +" found cur "+ (foundCur?.Data.ToString() ?? "none"));
-        //Check if there are cycles
-        if(list.AssertNoCycles()) Console.WriteLine("There are no cycles");
-        else Console.WriteLine("There are cycles");
-        // Make a cycle
-        list.Find(431253).Item2.Next = list.Find(43).Item2;
-        list.Count++;
-        //Check 
-        if(list.AssertNoCycles()) Console.WriteLine("There are no cycles");
-        else Console.WriteLine("There are cycles");
-
+        CheckSingleLinkedList();
+        CheckDoubleLinkedList();
+        CheckQueue();
+      
+        
 
     }
+
+    public static void CheckSingleLinkedList()
+    {
+        Console.WriteLine("Check Single Linked List");
+        SinglyLinkedList<int> listSingle = new();
+        listSingle.InsertAfter(null, 40);
+        listSingle.InsertAfter(listSingle.Last, 43);
+        listSingle.InsertAfter(listSingle.Last, 42433);
+        listSingle.InsertAfter(listSingle.Last, 431253);
+        listSingle.InsertAfter(listSingle.Last, 4343);
+        listSingle.InsertAfter(listSingle.Last, 43123);
+        Console.WriteLine("Printing all elements");
+        listSingle.PrintAll();
+        (SinglyLinkedList<int>.Node foundPrev,SinglyLinkedList<int>.Node foundCur) = listSingle.Find(43123);
+        Console.WriteLine("found prev - "+(foundPrev?.Data.ToString() ?? "none") +" found cur "+ (foundCur?.Data.ToString() ?? "none"));
+        // Removing element
+        Console.WriteLine("Removing element");
+        listSingle.RemoveAfter(listSingle.Find(431253).Item2);
+        listSingle.PrintAll();
+        //Check if there are cycles
+        if(listSingle.AssertNoCycles()) Console.WriteLine("There are no cycles");
+        else Console.WriteLine("There are cycles");
+        // Make a cycle
+        listSingle.Find(431253).Item2.Next = listSingle.Find(43).Item2;
+        //Check 
+        if(listSingle.AssertNoCycles()) Console.WriteLine("There are no cycles");
+        else Console.WriteLine("There are cycles");
+
+    }
+
+    public static void CheckDoubleLinkedList()
+    {
+        Console.WriteLine("Check Double Linked List");
+        DoublyLinkedList<int> listDouble = new();
+        listDouble.InsertBefore(null,43);
+        listDouble.InsertBefore(listDouble.First,45);
+        listDouble.InsertBefore(listDouble.First,46);
+        listDouble.InsertBefore(listDouble.First,47);
+        listDouble.InsertBefore(listDouble.First,49);
+        //Removing Elements
+        Console.WriteLine("Printing all elements");
+        listDouble.PrintAll();
+        listDouble.Remove(listDouble.Find(46).Item2);
+        listDouble.Remove(listDouble.Find(49).Item2);
+        Console.WriteLine("After removing 2 elements");
+        listDouble.PrintAll();
+    }
+
+    public static void CheckQueue()
+    {
+        Console.WriteLine("Check Queue");
+        Queue queue = new();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(5);
+        Console.WriteLine("Printing queue");
+        queue.PrintAll();
+
+        Console.WriteLine("Dequeuing Test:");
+        while (!queue.IsEmpty())
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+
+        Console.WriteLine("Queue is empty");
+    }
+
 }
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,3 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    public static void Main()
+    {
+        // Пример создания направленного графа
+        GraphNode node1 = new GraphNode(1);
+        GraphNode node2 = new GraphNode(2);
+        GraphNode node3 = new GraphNode(3);
+        GraphNode node4 = new GraphNode(4);
+        node1.Connect(node2, true);
+        node2.Connect(node3, true);
+        node3.Connect(node4, true);
+        node4.Connect(node2, false);
+        node4.Connect(node1, true);
+        List<GraphNode> nodes = [node1, node2, node3, node4];
+        List<bool> visitedDFS = Enumerable.Repeat(false, nodes.Count).ToList();
+        List<bool> visitedBFS = Enumerable.Repeat(false, nodes.Count).ToList();
 
-Console.WriteLine("Hello, World!");
+        // Start DFS from node1
+        Console.WriteLine("DFS:");
+        DFS(node1, visitedDFS);
+
+        // Start BFS from node1
+        Console.WriteLine("BFS:");
+        BFS(node1, visitedBFS);
+        
+        int sum = CalculateNeighborsSum(node1);
+        Console.WriteLine("Sum of neighbors: " + sum);
+    }
+
+    public static void DFS(GraphNode node, List<bool> visited)
+    {
+        Console.WriteLine($"Visited node with value {node.Index}");
+        visited[node.Index - 1] = true; 
+
+        foreach (GraphNode neighbor in node.Neighbors)
+        {
+            if (!visited[neighbor.Index - 1])
+            {
+                DFS(neighbor, visited);
+            }
+        }
+    }
+    public static void BFS(GraphNode startNode, List<bool> visited)
+    {
+        Queue<GraphNode> queue = new Queue<GraphNode>();
+        queue.Enqueue(startNode);
+        visited[startNode.Index - 1] = true; 
+
+        while (queue.Count > 0)
+        {
+            GraphNode current = queue.Dequeue();
+            Console.WriteLine($"Visited node with value {current.Index}");
+
+            foreach (GraphNode neighbor in current.Neighbors)
+            {
+                if (!visited[neighbor.Index - 1])
+                {
+                    visited[neighbor.Index - 1] = true;
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }
+    }
+    public static int CalculateNeighborsSum(GraphNode node)
+    {
+        int sum = 0;
+
+        foreach (GraphNode neighbor in node.Neighbors)
+        {
+            sum += neighbor.Index;
+        }
+
+        return sum;
+    }
+}
+
+public class GraphNode
+{
+    public int Index { get; set; }
+    public List<GraphNode> Neighbors { get; set; }
+
+    public GraphNode(int value)
+    {
+        Index = value;
+        Neighbors = new List<GraphNode>();
+    }
+
+
+    public GraphNode Connect(GraphNode to,bool directed = false)
+    {
+        this.Neighbors.Add(to);
+        if(!directed) to.Neighbors.Add(this);
+        return this;
+    }
+}
