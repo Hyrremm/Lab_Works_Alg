f99fffc98e6dcde41e092e35800d227ff7e25a0f
Hyrremm <abyssinianconq@gmail.com>
2024-03-15 16:15:04 +0300

    fin 1-2

--- Lab2_2/Program.cs
+++ Lab2_2/Program.cs
@@ -15,9 +15,8 @@ class Program{
         Array.Copy(sortedArray, reversedArray, sortedArray.Length);
         Array.Reverse(reversedArray);
         BinarySearchTree<int> binarySearchTree = new();
-        
-        WarmUp();   
-        
+
+        WarmUp();
         Stopwatch stopwatch = new();
         stopwatch.Start();
         binarySearchTree.PutValuesIntoBinaryTree(unsortedArray);
@@ -26,49 +25,53 @@ class Program{
 // Console.WriteLine("unsorted \n"+string.Join(" ",unsortedArray));
 // Console.WriteLine("sorted \n"+string.Join(" ",sortedArray));
 // Console.WriteLine("BinarySearchTree");
-            // binarySearchTree.PrintTree();
-        TimeSpan linearSum = TimeSpan.Zero;
-        for (int i = 0; i < numOfTries; i++)
+        // binarySearchTree.PrintTree();
         {
-            if(i%2==0) linearSum+= DetermineTimeOfSearchAlgorithm(LinearSearch,unsortedArray,false);
-            else linearSum+= DetermineTimeOfSearchAlgorithm(LinearSearch,unsortedArray,true);
+
         }
-
-        Console.WriteLine($"Average time for LinearSearch with length {arrLength} == {(linearSum/numOfTries).TotalMicroseconds} MICROSECONDS");
-        TimeSpan binarySum = TimeSpan.Zero;
-        for (int i = 0; i < numOfTries; i++)
-        {
-            if(i%2==0) binarySum+= DetermineTimeOfSearchAlgorithm(BinarySearch,sortedArray,false);
-            else binarySum+= DetermineTimeOfSearchAlgorithm(BinarySearch,sortedArray,true);        }
-        Console.WriteLine($"Average time for BinarySearch with length {arrLength} == {(binarySum/numOfTries).TotalMicroseconds} MICROSECONDS");
-
-        TimeSpan jumpSum = TimeSpan.Zero;
-        for (int i = 0; i < numOfTries; i++)
-        {
-            if(i%2==0) jumpSum+= DetermineTimeOfSearchAlgorithm(JumpSearch,sortedArray,false);
-            else jumpSum+= DetermineTimeOfSearchAlgorithm(JumpSearch,sortedArray,true);        }
-        Console.WriteLine($"Average time for JumpSearch with length {arrLength} == {(jumpSum/numOfTries).TotalMicroseconds} MICROSECONDS");
-
+        AssessAlgorithm(LinearSearch,unsortedArray,numOfTries,reversable:true);
+        AssessAlgorithm(BinarySearch,sortedArray,numOfTries,reversable:false);
+        AssessAlgorithm(JumpSearch,sortedArray,numOfTries,reversable:true);
         TimeSpan binaryTreeSum = TimeSpan.Zero;
         for (int i = 0; i < numOfTries; i++)
         {
             binaryTreeSum+= DetermineTimeOfSearchAlgorithm(binarySearchTree,unsortedArray);
         }
         Console.WriteLine($"Average time for BinaryTreeSearch with length {arrLength} == {(binaryTreeSum/numOfTries).TotalMicroseconds} MICROSECONDS");
-
     }
 
-
+    static void AssessAlgorithm<T>(Func<T[],T,bool,int> Algorithm,T[] arr,int numOfTries,bool reversable=false)
+    {
+        if (reversable)
+        {
+            TimeSpan jumpSum = TimeSpan.Zero;
+            for (int i = 0; i < numOfTries; i++)
+            {
+                if(i%2==0) jumpSum+= DetermineTimeOfSearchAlgorithm(Algorithm,arr,false);
+                else jumpSum+= DetermineTimeOfSearchAlgorithm(Algorithm,arr,true);
+                
+            }
+            Console.WriteLine($"Average time for JumpSearch with length {arr.Length} == {(jumpSum/numOfTries).TotalMicroseconds} MICROSECONDS");
+        }
+        else
+        {
+            TimeSpan binaryTreeSum = TimeSpan.Zero;
+            for (int i = 0; i < numOfTries; i++)
+            {
+                binaryTreeSum+= DetermineTimeOfSearchAlgorithm(Algorithm,arr);
+            }
+            Console.WriteLine($"Average time for BinaryTreeSearch with length {arr.Length} == {(binaryTreeSum/numOfTries).TotalMicroseconds} MICROSECONDS");
+        }
+    }
 static TimeSpan DetermineTimeOfSearchAlgorithm<T>(Func<T[],T,bool,int> Search,T[] arr,bool reversed=false)
 {
     Random random = new();
     int value = random.Next(0, arr.Length);
-    Stopwatch stopwatch = new();
-    stopwatch.Start();
+    var start = Stopwatch.GetTimestamp();
     int answer = Search(arr, arr[value], reversed);
-    stopwatch.Stop();
+    var end = Stopwatch.GetTimestamp();
+    return new(end - start);
     // Console.WriteLine($"Time spent doing {Search.GetMethodInfo().Name} : {stopwatch.Elapsed.ToString("g")} index - {answer}");
-    return stopwatch.Elapsed;
    
 
 }
@@ -111,10 +114,9 @@ static void WarmUp()
     BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
     binarySearchTree.PutValuesIntoBinaryTree(arr);
     LinearSearch(arr, arr[0],false);
-    BinarySearch(arr, arr[0],false);
+    BinarySearch(arr, arr[0]);
     JumpSearch(arr, arr[0],false);
     LinearSearch(arr, arr[0],true);
-    BinarySearch(arr, arr[0],true);
     JumpSearch(arr, arr[0],true);
     
     binarySearchTree.Search(arr[0]);
@@ -142,4 +144,5 @@ static int[] GenerateSortedArray(int len)
     }
     return unorderedSet.ToArray();
 }
+
 }
