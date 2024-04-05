716e761a901718a3c49b4b0be4ec5ad1bbaa84da
hyrremm <abyssinianconq@gmail.com>
2024-04-05 11:14:38 +0300

    пф

---
+++ Lab4/GraphNode.cs
@@ -0,0 +1,19 @@
+﻿public class GraphNode
+{
+    public int Index { get; set; }
+    public List<GraphNode> Neighbors { get; set; }
+
+    public GraphNode(int value)
+    {
+        Index = value;
+        Neighbors = new List<GraphNode>();
+    }
+
+
+    public GraphNode Connect(GraphNode to,bool directed = false)
+    {
+        this.Neighbors.Add(to);
+        if(!directed) to.Neighbors.Add(this);
+        return this;
+    }
+}
